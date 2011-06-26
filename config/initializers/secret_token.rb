53a86a42fd9fabc4b76065a6e1b3ad0c79a4f042
kopytek <tylkonieto1@gmail.com>
2011-06-26 20:17:26 +0200

    clean rails structure + html5 boilerplate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Notatnik::Application.config.secret_token = 'a863238d1ede7d3c9d346cfddb0041a190c0ad1c3dd7da3246aad146ef31263fef9f6bd09f1032a1b2af9b70790f8d5cf64918aaddb4d4242f7027b53ad0e721'
