55821e977c7417ccd1ad2fd9ece30c9a2f47d09f
kopytek <tylkonieto1@gmail.com>
2011-07-24 01:43:05 +0200

    css visible hidden instead of hiding with js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,8 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function () {
-    $('div.content').css('visibility','hidden');
-    $('input[type="submit"]').css('visibility','hidden');
+   // $('div.content').css('visibility','hidden');
+   // $('input[type="submit"]').css('visibility','hidden');
     
     $('input[type="text"].title').focus(function () {
        // $('article.entry').css('height', 'auto');
